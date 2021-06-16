8431cc7a6dadf56c7ef712b2910d882f40a8facc
Hoang Tam <goddie9x@yahoo.com>
2021-06-16 15:48:55 +0700

    update interval check played and  add push lo de

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,5 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-const Oder = ['Đặc biệt', 'Giải nhất', 'Giải nhì', 'Giải ba', 'Giải bốn', 'Giải năm', 'Giải sáu', 'Giải bảy'];
-
 const $AO = function activeOn(elements) {
     elements.forEach(element => {
         element.classList.add('active');
@@ -17,6 +15,8 @@ const $toggle = function toggleActiveClass(elements) {
         element.classList.toggle('active');
     })
 }
+let currentMoney = 1000000;
+let played = false;
 
 $('.main-nav-mobile').onclick = function() {
     $toggle([...$$('.menu-icon'), $('.main-nav-bar-mobile')]);
@@ -25,58 +25,126 @@ $('.main-nav-mobile').onclick = function() {
 start();
 
 async function start() {
-    createScores('input[value="Oánh lại"]', '.xoso', 'input[value="Tắt âm"]')
-        .then(prizes => {
-            console.log(prizes)
-        })
+    let prizes = ['', '', '', '', '', '', '', ''];
+    acting('.lo input[value="phang"]', '.de input[value="phang"]');
+
+    createScores('input[value="Oánh lại"]', '.xoso', 'input[value="Đặt lại"]', 'input[value="Tắt âm"]');
+    let checkPlayDone = setInterval(function() {
+        if (played) {
+            for (let i = 0; i < 8; i++) {
+                prizes[i] = getScores(i);
+            }
+            checkPlayDone.clearInterval();
+        }
+    }, 1000)
 }
 
+function acting(btnLo, btnDe) {
+    let buttonLo = $(btnLo);
+    let buttonDe = $(btnDe);
 
-async function createScores(buttonPlay, musicPlay, buttonStopMusic) {
-    let prizes = [];
-    let waitingTimes = 0;
-    if (buttonStopMusic) {
-        $(buttonStopMusic).onclick = function() {
+    buttonLo.onclick = function(e) {
+        handleActing('lo');
+    }
+    buttonDe.onclick = function(e) {
+        handleActing('de');
+    }
+}
+
+function handleActing(type) {
+    let numberInput = $(`#select-${type}`);
+    let moneyInput = $(`#money-${type}`);
+    const acting = $(`.acting-${type}`);
+
+    if (numberInput && numberInput) {
+        let number = numberInput.value;
+        let money = moneyInput.value;
+
+        if (number && money) {
+            currentMoney -= money;
+            acting.innerHTML += ` <div class="acting-times col">
+            <div class="nbr">${number}:</div>
+            <div class="mney">${money}</div>
+            <div>`;
+            return {
+                number,
+                money
+            }
+        } else {
+            alert('vui lòng nhập đủ cả số đánh và cả tiền');
+            return;
+        }
+    }
+}
+
+function calMoneys(prizes) {
+
+}
+
+async function createScores(buttonPlay, musicPlay, buttonReset, buttonStopMusic) {
+
+    const playButton = $(buttonPlay);
+    let stopMusicBtn = (buttonStopMusic) ? ($(buttonStopMusic)) : (undefined);
+    let musicPlayBtn = (musicPlay) ? ($(musicPlay)) : (undefined);
+    let resetScoresBtn = (buttonReset) ? ($(buttonReset)) : (undefined);
+
+    if (stopMusicBtn) {
+        stopMusicBtn.onclick = function() {
             if (musicPlay) {
                 $(musicPlay).pause();
             }
         };
     }
-    $(buttonPlay).onclick = async function() {
-        if (musicPlay) {
-            $(musicPlay).volume = 0.5;
-            $(musicPlay).play();
+    if (resetScoresBtn) {
+        resetScoresBtn.onclick = function() {
+            if (musicPlayBtn) {
+                musicPlayBtn.pause();
+            }
+            for (let i = 7; i >= 0; i--) {
+                resetScores(i);
+            }
+            $('.acting').innerHTML = `
+            <div class="flex-item">
+                        <div class="acting-lo-title">
+                            Danh sách lô đã oánh:
+                        </div>
+                        <div class="acting-lo"></div>
+                    </div>
+                    <div class="flex-item">
+                        <div class="acting-de-title">
+                            Danh sách đề đã oánh:
+                        </div>
+                        <div class="acting-de"></div>
+                    </div>
+            `;
+            played = false;
+        }
+    }
+    playButton.onclick = async function nope() {
+        if (musicPlayBtn) {
+            musicPlayBtn.volume = 0.5;
+            musicPlayBtn.play();
         }
         for (let i = 7; i >= 0; i--) {
-            getScores(i)
-                .then(prize => {
-                    prizes.push(prize)
-                })
+            createScore(i);
             let waitingTime = (i > 2) ? (7000 - Math.abs(4 - i) * 800) : (2000);
             let a = await delay(waitingTime);
         }
+        played = true;
     }
-    for (let i = 7; i >= 0; i--) {
-        let waitingTime = (i > 2) ? (7000 - Math.abs(4 - i) * 800) : (2000);
-        waitingTimes += waitingTime;
-    }
-    return new Promise(async(resolve) => {
-        setTimeout(resolve(prizes), waitingTimes);
-    })
 }
 
-function deleteScores(buttonReset) {
-    $(buttonReset).onclick = function() {
-        if (musicPlay) {
-            $(musicPlay).pause();
-        }
-        for (let i = 7; i >= 0; i--) {
-            prizes[i] = resetScores(i);
-        }
+function getScores(id) {
+    let prizes = [];
+    const scores = (id == 3 || id == 5) ? (document.getElementById(id).querySelectorAll('.flex-item:not(.title,.score)')) : (document.getElementById(id).querySelectorAll('.flex-item.score'));
+
+    for (const score of scores) {
+        prizes.push(score.innerHTML);
     }
+    return prizes;
 }
-async function getScores(id) {
-    let prize = [];
+
+async function createScore(id) {
     const length = (id < 4) ? (5) : ((id < 6) ? (4) : ((id < 7) ? (3) : (2)));
     const scores = (id == 3 || id == 5) ? (document.getElementById(id).querySelectorAll('.flex-item:not(.title,.score)')) : (document.getElementById(id).querySelectorAll('.flex-item.score'));
 
@@ -91,10 +159,11 @@ async function getScores(id) {
             score.innerHTML = tempScore;
             times++;
         }, 50);
-        prize.push(tempScore);
         let a = await delay(1000);
     }
-    return Promise.resolve(prize);
+    return await new Promise(resolve => {
+        resolve()
+    });
 }
 
 function resetScores(id) {
@@ -129,4 +198,8 @@ function delay(ms) {
     return new Promise(resolve => {
         setTimeout(resolve, ms);
     })
+}
+
+$('input[value="Xem thống kê"]').onclick = function() {
+    $('.cal').style.display = 'block';
 }
